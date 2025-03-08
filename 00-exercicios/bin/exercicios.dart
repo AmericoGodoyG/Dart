9649c433c2d424fcb444c66141545a79db4eed12
Américo Godoy da Silva <160660569+AmericoGodoyG@users.noreply.github.com>
2025-03-08 16:09:43 -0300

    Update exercicios.dart

--- 00-exercicios/bin/exercicios.dart
+++ 00-exercicios/bin/exercicios.dart
@@ -97,5 +97,147 @@ void main(List<String> arguments) {
   int cupons = (valorEmCompras / 50).floor();
 
   print('O cliente receberá $cupons cupom(s) para esta compra.');
+ // Exercicio 9
+  print("Digite sua idade: ");
 
+  // Tratando tipos
+  String? entradaIdade = stdin.readLineSync();
+  
+  if (entradaIdade != null) {
+    if (entradaIdade != "") {
+
+      int idade = int.parse(entradaIdade);
+
+      if (idade >= 18) {
+
+        print("Habilitação permitida!");
+      } else {
+
+        print("Habilitação não permitida");
+    }
+  }
+
+  // Exercicio 10
+  print("Digite um número de 1 a 12");
+  String? entradaMes = stdin.readLineSync();
+  if (entradaMes != null) {
+
+    if(entradaMes != ""){
+
+      int mes = int.parse(entradaMes);
+
+      switch(mes){
+        case 1:
+          print("Janeiro");
+          break;
+        case 2:
+          print("Fevereiro");
+          break;
+        case 3:
+          print("Março");
+          break;
+        case 4:
+          print("Abril");
+          break;
+        case 5:
+          print("Maio");
+          break;
+        case 6:
+          print("Junho");
+          break;
+        case 7:
+          print("Julho");
+          break;
+        case 8:
+          print("Agosto");
+          break;
+        case 9:
+          print("Setembro");
+          break;
+        case 10:
+          print("Outubro");
+          break;
+        case 11:
+          print("Novembro");
+          break;
+        case 12:
+          print("Dezembro");
+          break;
+        default:
+          print("Valor inválido");
+      }
+    }
+  } 
+
+  // Exercicio 11
+  double saldo = 1000.0; // Saldo inicial em reais
+
+  print('Boas-vindas ao seu banco digital!');
+  print('Seu saldo atual é de: R\$${saldo.toStringAsFixed(2)}');
+
+  print('Digite o valor do Pix que deseja realizar:');
+
+  String? entradaPix = stdin.readLineSync();
+
+  // Tratando null e empty
+  if (entradaPix != null) {
+
+    if (entradaPix != "") {
+
+      double valorPix = double.parse(entradaPix);
+      
+      if (valorPix <= saldo) {
+        print("Realizando Pix...");
+        print("Saldo atual: R\$${saldo - valorPix}");
+      }
+
+    }
+
+  }
+  print("Operação finalizada!");
+
+  // Exercicio 12
+  print("Digite sua idade: ");
+  String? idadeEntrada = stdin.readLineSync();
+
+  print("Digite o país em que você está [usa] [br] [jp]: ");
+  String? pais = stdin.readLineSync();
+
+  if (idadeEntrada != null && pais != null) {
+    if (idadeEntrada != "" && pais != "") {
+
+      int idade = int.parse(idadeEntrada);
+
+      switch(pais){
+
+        case "usa":
+          if (idade >= 16) {
+            print("[USA] Habilitação permitida. Idade: $idade");
+          } else {
+            print("[USA] Habilitação negada. Idade: $idade");
+          }
+        break;
+
+        case "br":
+          if (idade >= 18) {
+            print("[BR] Habilitação permitida. Idade: $idade");
+          } else {
+            print("[BR] Habilitação negada. Idade: $idade");
+          }
+        break;
+    
+        case "jp":
+          if (idade >= 20) {
+            print("[JP] Habilitação permitida. Idade: $idade");
+          } else {
+            print("[JP] Habilitação negada. Idade: $idade");
+          }
+        break;
+
+        default:
+          print("País não encontrado!");
+      }
+    }
+  }
+}
 }
